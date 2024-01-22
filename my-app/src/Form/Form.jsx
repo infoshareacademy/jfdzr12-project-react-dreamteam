936d4813593f629eac2e4f9f8cf470dbc2d6374f
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-01-22 02:19:40 +0100

    update Form.jsx $ added Form.module.css

--- my-app/src/Form/Form.jsx
+++ my-app/src/Form/Form.jsx
@@ -1,107 +1,109 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import styles from './Form.module.css'
 
-const Form = () => {
+const contactForm = () => {
+  const [form, setForm] = useState({
+    title: '',
+    email: '',
+    message: '',
+  });
 
-    const [formData, setFormData] = useState({
-        title: '',
-        email: '',
-        message: '',
-      });
-    
-      const [errors, setErrors] = useState({
-        title: '',
-        email: '',
-        message: '',
-      });
-    
-      const validateEmail = (email) => {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return emailRegex.test(email);
-      };
-    
-      const validateForm = () => {
-        let isValid = true;
-        const newErrors = { title: '', email: '', message: '' };
-    
-        if (formData.title.trim() === '') {
-          isValid = false;
-          newErrors.title = 'Tytuł jest wymagany';
-        }
-    
-        if (!validateEmail(formData.email)) {
-          isValid = false;
-          newErrors.email = 'Nieprawidłowy adres email';
-        }
-    
-        if (formData.message.trim().length < 20) {
-          isValid = false;
-          newErrors.message = 'Treść wiadomości musi zawierać co najmniej 20 znaków';
-        }
-    
-        setErrors(newErrors);
-        return isValid;
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        if (validateForm()) {
-          // Tutaj możesz obsłużyć logikę wysyłki formularza
-          console.log('Formularz wysłany:', formData);
-        } else {
-          console.log('Formularz zawiera błędy');
-        }
-      };
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-          ...prevData,
-          [name]: value,
-        }));
-      };
-    return (
-        
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="title">Tytuł:</label>
-                <input
-                type="text"
-                id="title"
-                name="title"
-                value={formData.title}
-                onChange={handleChange}
-                />
-                <div className="error">{errors.title}</div>
-            </div>
+  const [errors, setErrors] = useState({
+    title: '',
+    email: '',
+    message: '',
+  });
 
-            <div>
-                <label htmlFor="email">E-mail nadawcy:</label>
-                <input
-                type="text"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                />
-                <div className="error">{errors.email}</div>
-            </div>
+  const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
 
-            <div>
-                <label htmlFor="message">Treść wiadomości:</label>
-                <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                />
-                <div className="error">{errors.message}</div>
-            </div>
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = { title: '', email: '', message: '' };
 
-            <button type="submit">Wyślij</button>
-            </form>
-    )
+    if (form.title.trim() === '') {
+      isValid = false;
+      newErrors.title = 'Title is required';
+    }
 
+    if (!validateEmail(form.email)) {
+      isValid = false;
+      newErrors.email = 'Invalid email address';
+    }
+
+    if (form.message.trim().length < 20) {
+      isValid = false;
+      newErrors.message = 'Min. message length required: 20 characters';
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (validateForm()) {
+      console.log('Form sent:', form);
+    } else {
+      console.log('The form contains errors');
+    }
+  }
+
+  const handleChangeValue = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <form className={styles.form__card} onSubmit={handleSubmit}>
+      <div className={styles.form__inputName}>
+        <label htmlFor="title">Title:</label>
+      </div>
+      <div>
+        <input
+          className={styles.form__input}
+          type="text"
+          id="title"
+          name="title"
+          value={form.title}
+          onChange={handleChangeValue}
+        />
+        <div className={styles.error}>{errors.title}</div>
+      </div>
+
+      <div className={styles.form__inputName}>
+        <label htmlFor="email">E-mail:</label>
+      </div>
+      <div>
+        <input
+          className={styles.form__input}
+          type="email"
+          id="email"
+          name="email"
+          value={form.email}
+          onChange={handleChangeValue}
+        />
+        <div className={styles.error}>{errors.email}</div>
+      </div>
+
+      <div className={styles.form__inputName}>
+        <label htmlFor="message">Message:</label>
+      </div>
+      <div>
+        <textarea
+          className={styles.form__input}
+          id="message"
+          name="message"
+          value={form.message}
+          onChange={handleChangeValue}
+        />
+        <div className={styles.error}>{errors.message}</div>
+      </div>
+
+      <button className={styles.form__btn} type="submit">Send</button>
+    </form>
+  )
 }
 
-    export default Form;
+export default contactForm
