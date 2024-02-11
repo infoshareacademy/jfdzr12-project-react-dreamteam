2a0671e38c75d79ac9d6980f702663a9bbf20c2b
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-11 12:22:59 +0100

    image.js  & css structure from index.html & style.css

--- my-app/src/Gallery/Image.jsx
+++ my-app/src/Gallery/Image.jsx
@@ -1,13 +1,27 @@
 
 export const Image = () => {
-    const {id} = useParams();
+    const { id } = useParams();
 
-return (
+    return (
 
-    return <div>
-zdjęcie
+    return
+//zdjęcie
 
-    </div>
+    // <form action="javascript:;" id="productCard" class="product__card">
+    //   <input type="image" alt="cat" src="#" class="form__img"/>
+    //   <div class="form_card">
+    //     <div class="form_information">
+    //       <button class="form__close--btn"><i class="fa-solid fa-xmark form-close"></i></button>
+    //       <h2 id="formTitle" class="form__title">Title</h2>
+    //       <p id="formPrice" class="form__price">$10</p>
+    //       <span id="formHash1" class="form__hash">#tag1</span>
+    //       <span id="formHash2" class="form__hash">#tag2</span>
+    //       <span id="formHash3" class="form__hash">#tag3</span>
+    //     </div>
+    //     <button type="submit" id="formBuy" class="form__buy">Add to basket</button>
+    //   </div>
+    // </form>
+
 
 )
    
