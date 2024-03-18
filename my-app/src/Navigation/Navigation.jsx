55cc37e8e72b877571ad8fb91cfcb2e91d957e56
Aleksandra Boroń <alek166194@wgmail.com>
2024-03-18 18:41:58 +0100

    On branch aleksandra-boron

--- my-app/src/Navigation/Navigation.jsx
+++ my-app/src/Navigation/Navigation.jsx
@@ -11,9 +11,9 @@ export const Navigation = ({ user }) => {
   // dodany useContext do zmiany kolorystyki, useState do obsługi przycisku zmieniającego motywy kolorystyczne,
   //  i zmienne (stałe), które zamieniają kilka klas css na stringi; są wykorzystane jako warunkowe classNames 
 
-  // const { setTheme } = useContext(ThemeContext);
+  const { setTheme } = useContext(ThemeContext);
 
-  // const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const buttonText = isDarkMode ? 'Light' : 'Dark';
 
